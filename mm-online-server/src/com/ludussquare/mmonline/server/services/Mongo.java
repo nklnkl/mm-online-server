66556cbcd9a22bb01a91934c7a44812b088bd91b
Niko <Niko>
2016-12-07 11:50:34 -0500

    Added mongo properties to gitignore. Set mongo service file to use properties file.

--- mm-online-server/src/com/ludussquare/mmonline/server/services/Mongo.java
+++ mm-online-server/src/com/ludussquare/mmonline/server/services/Mongo.java
@@ -1,5 +1,13 @@
 package com.ludussquare.mmonline.server.services;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
@@ -14,13 +22,33 @@ public class Mongo {
 	private MongoClientURI uri;
 	private String db;
 	
+	private Properties properties;
+	private InputStream input;
+	
 	public Mongo () {
+		// Get mongo config from properties file.
+		properties = new Properties();
+		
+		try {
+			input = new FileInputStream("Mongo.properties");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			properties.load(input);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		// Creates instance for morphia ORM.
 		morphia = new Morphia();
 		// Create mapping of models.
 		morphia.mapPackage("com.ludussquare.mmonline.server.models");
 		// Set the URI & DB.
-		uri = new MongoClientURI("mongodb://mm-online:mmonline@ds157677.mlab.com:57677/mm-online-db");
+		uri = new MongoClientURI(properties.getProperty("MongoUri"));
 		db = "mm-online-db";
 		// Use the URI to connect to the db.
 		client = new MongoClient(uri);
